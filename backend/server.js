9040967c229eb0e1e8bce24cfd1b9b9c349fc398
vipul <vipul03pandey@gmail.com>
2025-08-11 16:07:50 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,21 @@ app.get("/", (req, res) => {
 });
 
 const PORT=process.env.PORT || 5000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173', // for local dev
+  'https://project-assignment-git-main-vip03pandeys-projects.vercel.app' 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 
 app.use("/api/users", userRoutes);
